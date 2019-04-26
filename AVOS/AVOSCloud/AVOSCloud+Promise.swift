e575a1a84ed592d156009b98b1ec3f81541d28c9
hustlzp <hustlzp@qq.com>
2019-04-26 20:22:17 +0800

    Update

--- AVOS/AVOSCloud/AVOSCloud+Promise.swift
+++ AVOS/AVOSCloud/AVOSCloud+Promise.swift
@@ -33,20 +33,37 @@ extension AVCloud {
     ///   - maxCacheAge: <#maxCacheAge description#>
     ///   - parameters: <#parameters description#>
     /// - Returns: <#return value description#>
-    public class func rpcFunction<T>(_: PMKNamespacer, name: String, cachePolicy: AVCachePolicy, maxCacheAge: TimeInterval, parameters: [String: Any?]?) -> Promise<T> {
+    public class func rpcFunction<T>(_: PMKNamespacer, name: String, cachePolicy: AVCachePolicy, maxCacheAge: TimeInterval, parameters: [String: Any?]?, type: T.Type) -> Promise<T> {
         return Promise { resolver in
             rpcFunction(inBackground: name, withParameters: parameters, cachePolicy: cachePolicy, maxCacheAge: maxCacheAge, block: { (result, fromCache, error) in
                 guard error == nil else {
                     resolver.reject(error!)
                     return
                 }
-                
+
                 guard let object = result as? T else {
                     resolver.reject(NSError(domain: kLeanCloudErrorDomain, code: kAVErrorIncorrectType, userInfo: [NSLocalizedDescriptionKey: "Return data's type should be \(T.self)"]))
                     return
                 }
                 
                 resolver.fulfill(object)
+            })
+        }
+    }
+
+    /// RPC 调用云函数
+    ///
+    /// - Parameters:
+    ///   - _: <#_ description#>
+    ///   - name: <#name description#>
+    ///   - cachePolicy: <#cachePolicy description#>
+    ///   - maxCacheAge: <#maxCacheAge description#>
+    ///   - parameters: <#parameters description#>
+    /// - Returns: <#return value description#>
+    public class func rpcFunction(_: PMKNamespacer, name: String, cachePolicy: AVCachePolicy, maxCacheAge: TimeInterval, parameters: [String: Any?]?) -> Promise<Any> {
+        return Promise { resolver in
+            rpcFunction(inBackground: name, withParameters: parameters, cachePolicy: cachePolicy, maxCacheAge: maxCacheAge, block: { (result, fromCache, error) in
+                resolver.resolve(result, error)
             })
         }
     }
