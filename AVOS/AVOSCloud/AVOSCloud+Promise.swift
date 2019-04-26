2c4e45b8080425e079aa36908e53de401f79a3e9
hustlzp <hustlzp@qq.com>
2019-04-26 16:07:36 +0800

    Update.

--- AVOS/AVOSCloud/AVOSCloud+Promise.swift
+++ AVOS/AVOSCloud/AVOSCloud+Promise.swift
@@ -10,12 +10,29 @@
 import Foundation
 
 extension AVCloud {
+    
+    /// 调用云函数
+    ///
+    /// - Parameters:
+    ///   - _: <#_ description#>
+    ///   - name: <#name description#>
+    ///   - parameters: <#parameters description#>
+    /// - Returns: <#return value description#>
     public class func callFunction(_: PMKNamespacer, name: String, parameters: [String: Any]?) -> Promise<Any?> {
         return Promise {
             callFunction(inBackground: name, withParameters: parameters, block: $0.resolve)
         }
     }
     
+    /// RPC 调用云函数
+    ///
+    /// - Parameters:
+    ///   - _: <#_ description#>
+    ///   - name: <#name description#>
+    ///   - cachePolicy: <#cachePolicy description#>
+    ///   - maxCacheAge: <#maxCacheAge description#>
+    ///   - parameters: <#parameters description#>
+    /// - Returns: <#return value description#>
     public class func rpcFunction<T>(_: PMKNamespacer, name: String, cachePolicy: AVCachePolicy, maxCacheAge: TimeInterval,b parameters: [String: Any?]?) -> Promise<T> {
         return Promise { resolver in
             rpcFunction(inBackground: name, withParameters: parameters, cachePolicy: cachePolicy, maxCacheAge: maxCacheAge, block: { (result, fromCache, error) in
@@ -32,5 +49,90 @@ extension AVCloud {
                 resolver.fulfill(object)
             })
         }
+    }
+}
+
+public protocol PromiseAVObject { }
+
+extension AVObject: PromiseAVObject {
+}
+
+extension PromiseAVObject where Self: AVObject {
+    public func fetch(_: PMKNamespacer, withKeys keys: [String]? = nil) -> Promise<Self> {
+        return Promise<Self>(resolver: { (seal) in
+            fetchInBackground(withKeys: keys, block: { (result, error) in
+                seal.resolve(result as? Self, error)
+            })
+        })
+    }
+    
+    public func save(_: PMKNamespacer) -> Promise<Self> {
+        return Promise<Self>(resolver: { (seal) in
+            saveInBackground({ (succeeded, error) in
+                seal.resolve(self, error)
+            })
+        })
+    }
+}
+
+extension AVUser {
+    
+    /// 用户名登录
+    ///
+    /// - Parameters:
+    ///   - _: <#_ description#>
+    ///   - username: <#username description#>
+    ///   - password: <#password description#>
+    /// - Returns: <#return value description#>
+    public class func logInWithUsername(_: PMKNamespacer, _ username: String, password: String) -> Promise<AVUser> {
+        return Promise<AVUser>(resolver: { (seal) in
+            AVUser.logInWithUsername(inBackground: username, password: password, block: { (user, error) in
+                seal.resolve(user, error)
+            })
+        })
+    }
+    
+    /// 邮箱登录
+    ///
+    /// - Parameters:
+    ///   - _: <#_ description#>
+    ///   - email: <#email description#>
+    ///   - password: <#password description#>
+    /// - Returns: <#return value description#>
+    public class func loginWithEmail(_: PMKNamespacer, email: String, password: String) -> Promise<AVUser> {
+        return Promise<AVUser>(resolver: { (seal) in
+            AVUser.login(withEmail: email, password: password, block: { (user, error) in
+                seal.resolve(user, error)
+            })
+        })
+    }
+    
+    /// 请求验证邮箱
+    ///
+    /// - Parameters:
+    ///   - _: <#_ description#>
+    ///   - email: <#email description#>
+    /// - Returns: <#return value description#>
+    public class func requestEmailVerify(_: PMKNamespacer, email: String) -> Promise<Void> {
+        return Promise<Void>(resolver: { (resolver) in
+            AVUser.requestEmailVerify(email, with: { (succeeded, error) in
+                resolver.resolve(error)
+            })
+        })
+    }
+    
+    /// 发送密码重置邮件
+    ///
+    /// - Parameters:
+    ///   - _: <#_ description#>
+    ///   - email: <#email description#>
+    /// - Returns: <#return value description#>
+    public class func requestPasswordResetForEmail(_: PMKNamespacer, email: String) -> Promise<Void> {
+        return Promise<Void>(resolver: { (resolver) in
+            AVUser.requestPasswordResetForEmail(inBackground: email
+                , block: { (succeeded, error) in
+                resolver.resolve(error)
+            })
+        })
     }
 }
